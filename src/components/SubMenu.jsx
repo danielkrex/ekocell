758a8d5eef0eb647e1737ee7763955c2f513040f
danielkreko <danielkrex@gmail.com>
2023-12-09 16:14:46 +0100

    submenu fuix

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 
+// Check if window is defined (so if in the browser or in node.js).
+const isBrowser = typeof window !== "undefined"
+
 function SubMenu(props) {
-  const [location, setlocation] = useState(window.location.pathname);
+  const [location, setlocation] = useState( isBrowser ? window.location.pathname : '');
   const listItems = props.data.map((item, index) => (
     <a
       href={item.link}
